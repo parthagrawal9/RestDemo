df3928580410e54bcf82d27f75e6e407a8c46ea7
Siddharth Goyal <sid@Siddharths-MacBook-Pro.local>
2018-03-23 08:41:28 -0400

    jdbcTemplate

--- src/main/java/com/spring/rest/spring_rest/RestTestController.java
+++ src/main/java/com/spring/rest/spring_rest/RestTestController.java
@@ -1,14 +1,11 @@
 package com.spring.rest.spring_rest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,29 +37,6 @@ public class RestTestController {
 	@RequestMapping("/employees")
 	public List<Employee> getAllEmplloyeeData(){	
 		List<Employee> allEmployeeData= new ArrayList<Employee>();
-		Connection c = null;
-    	Statement stmt = null;
-	     try {
-	         Class.forName("org.postgresql.Driver");
-	         c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "root");
-	         stmt = c.createStatement();
-	         ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-	         while ( rs.next() ) {
-	            int id = rs.getInt("id");
-	            String  name = rs.getString("name");
-	            int age  = rs.getInt("age");
-	            String  address = rs.getString("address").trim();
-	            float salary = rs.getFloat("salary");
-	            allEmployeeData.add(new Employee(id,name,age,address,salary));
-	         }
-	         rs.close();
-	         stmt.close();
-	         c.close();
-	      } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-	         
-	         //System.exit(0);
-	      }
-		return allEmployeeData;
+		return jdbcTemplate.queryForList("SELECT * FROM COMPANY", Employee.class);
 	}
 }
